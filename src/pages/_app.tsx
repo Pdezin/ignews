7c14b7aac1499633b818ce6282aaebb656872d95
Pedro Jardim <pedro200219@outlook.com>
2022-02-05 14:21:47 -0300

    Autenticação Github

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { AppProps } from "next/app";
 import { Header } from "../components/Header";
 import "../styles/global.scss";
+import { SessionProvider as NextAuthProvider } from "next-auth/react";
 
 /**
  * _app é recarregado sempre que trocar de página
@@ -8,10 +9,10 @@ import "../styles/global.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <NextAuthProvider session={pageProps.session}>
       <Header />
       <Component {...pageProps} />
-    </>
+    </NextAuthProvider>
   );
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,29 @@
+import { GetStaticProps } from "next";
 import Head from "next/head";
 import { SubscribeButton } from "../components/SubscribeButton";
+import { stripe } from "../services/stripe";
 import styles from "./home.module.scss";
 
-export default function Home() {
+/**
+ * FORMAS CHAMADAS HTTP
+ * Client-Side - Outros casos algo que é carregado a uma ação do usuário, informações que não necessitam serem pre recarregados
+ * Server-Side - Indexaçao, dados dinâmicos, sessão do usuário , informações em tempo real do usuário contexto...
+ * Static Site Generation - Indexaçao, Performance, salvar conteúdo estático (que é o mesmo para todos os usuários)
+ *
+ * Post Blog
+ *
+ * Conteúdo (SSG)
+ * Comentário (Client-Side)
+ */
+
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  };
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -16,12 +37,41 @@ export default function Home() {
           </h1>
           <p>
             Get access to all publications <br />
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
         <img src="/images/avatar.svg" alt="Girl coding" />
       </main>
     </>
   );
 }
+
+/**
+ * Funções executadas no next
+ * getServerSideProps -> SSR
+ * getStaticProps -> SSG
+ */
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve("price_1KLcptKxIhwMYggjWlM6wWN9");
+  /**
+   * Como segundo parâmetro do retrive pode se passar {expand: ["product"]}
+   * Nesse caso além de recuperar o preço pegaria os outros dados do produto
+   */
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    }).format(price.unit_amount / 100),
+  };
+
+  return {
+    props: {
+      product,
+    },
+    revalidate: 60 * 60 * 24, // 24 horas (informar em minutos)
+  };
+};
